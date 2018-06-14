42b7635fdce269f178e2264c2d0f16e899bd5bb2
Mason Elmore <mason.elmore@gmail.com>
2018-06-13 20:48:43 -0500

    Add exercise 2.1

---
+++ ch2/ex2.1/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/masonelmore/gopl/ch2/ex2.1/tempconv"
+)
+
+func main() {
+	c := tempconv.Celsius(30)
+	f := tempconv.Fahrenheit(86)
+	k := tempconv.Kelvin(303.15)
+
+	fmt.Printf("%v = %v = %v\n", c, tempconv.CToF(c), tempconv.CToK(c))
+	fmt.Printf("%v = %v = %v\n", f, tempconv.FToC(f), tempconv.FToK(f))
+	fmt.Printf("%v = %v = %v\n", k, tempconv.KToC(k), tempconv.KToF(k))
+}
